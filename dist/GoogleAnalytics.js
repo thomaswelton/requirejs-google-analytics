d5a9125cb9b7064fc2e215cc5fbd1be9cd67fa9c
Thomas Welton <thomas@clicklabs.co.uk>
2013-07-25 12:02:39 +0100

    Added onReady and trackEvent methods

--- dist/GoogleAnalytics.js
+++ dist/GoogleAnalytics.js
@@ -1,13 +1,20 @@
 (function() {
-  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  define(['module'], function(module) {
+  define(['module', 'EventEmitter'], function(module, EventEmitter) {
     var GoogleAnalytics;
 
-    GoogleAnalytics = (function() {
+    GoogleAnalytics = (function(_super) {
+      __extends(GoogleAnalytics, _super);
+
       function GoogleAnalytics(config) {
         this.config = config;
+        this.trackEvent = __bind(this.trackEvent, this);
+        this.onReady = __bind(this.onReady, this);
         this.injectScript = __bind(this.injectScript, this);
+        GoogleAnalytics.__super__.constructor.call(this);
         console.log('GoogleAnalytics Class', this.config);
         requirejs.config({
           paths: {
@@ -26,14 +33,34 @@
         var _this = this;
 
         return requirejs(['ga'], function(ga) {
+          _this.ga = ga;
           ga('create', _this.config.id);
-          return ga('send', 'pageview');
+          ga('send', 'pageview');
+          return _this.fireEvent('gaReady', ga);
+        });
+      };
+
+      GoogleAnalytics.prototype.onReady = function(cb) {
+        var _this = this;
+
+        if (this.ga != null) {
+          return cb(this.ga);
+        } else {
+          return this.once('gaReady', function(ga) {
+            return cb(ga);
+          });
+        }
+      };
+
+      GoogleAnalytics.prototype.trackEvent = function(category, action, label, value) {
+        return this.onReady(function(ga) {
+          return ga('send', 'event', category, action, label, value);
         });
       };
 
       return GoogleAnalytics;
 
-    })();
+    })(EventEmitter);
     return new GoogleAnalytics(module.config());
   });
 
--- src/GoogleAnalytics.coffee
+++ src/GoogleAnalytics.coffee
@@ -1,6 +1,8 @@
-define ['module'], (module) ->
-	class GoogleAnalytics 
+define ['module', 'EventEmitter'], (module, EventEmitter) ->
+	class GoogleAnalytics extends EventEmitter 
 		constructor: (@config) ->
+			super()
+
 			console.log 'GoogleAnalytics Class', @config
 
 			requirejs.config
@@ -11,12 +13,24 @@ define ['module'], (module) ->
 						exports: 'ga'
 
 			@injectScript()
-				
 
 		injectScript: (cb) =>
-			requirejs ['ga'], (ga) =>
+			requirejs ['ga'], (@ga) =>
 				ga 'create', @config.id
 				ga 'send', 'pageview'
+
+				@fireEvent 'gaReady', ga
+
+		onReady: (cb) =>
+			if @ga?
+				cb @ga
+			else
+				@once 'gaReady', (ga) =>
+					cb ga
+
+		trackEvent: (category, action, label, value ) =>
+			@onReady (ga) ->
+				ga('send', 'event', category, action, label, value)
 
 	## Create and return a new instance of GoogleAnalytics
 	## module.config() returns a JSON object as defined in requirejs.config.GoogleAnalytics
